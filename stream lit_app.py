59f77b63bc8a22126e2f0c76a430983ee476a2ec
DavidReid248 <david.reid1@student.mec.cuny.edu>
2025-11-03 10:57:43 -0500

    Update stream lit_app.py

--- stream lit_app.py
+++ stream lit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
+
+# page config
+st.set_up_config(
+  page_title='David Reid | portfolio', 
+  page_icon='👌',
+  layout = 'wide'
+)
+
