7268af438130007e4635446901dc5012f38c10d6
DavidReid248 <david.reid1@student.mec.cuny.edu>
2025-11-22 17:41:28 -0500

    Update stream lit_app.py

--- stream lit_app.py
+++ stream lit_app.py
@@ -21,3 +21,9 @@ st.markdown('''
 st.sidebar.title('👔 Navigation')
 page = st.sidebar.radio('Go to' ,
                         ['.🛖 Home', '🙋‍♂️ About', '💼 Projects', '🛠 Skills' ,'📝 Resume', '📩 Contact' ])
+
+# Home Page
+if page == '🛖 Home':
+  st.markdown('<p class="main-header">David Reid</p>', unsafe_allow_html=True)
+  st.markdown('<p class="sub-header">David Reid Student | Medgar Evers College</p>', unsafe_allow_html=True)
+  
