66c524c524a0f51720c4397a5e822dc97fb1adc2
DavidReid248 <david.reid1@student.mec.cuny.edu>
2025-11-22 16:34:02 -0500

    Update stream lit_app.py

--- stream lit_app.py
+++ stream lit_app.py
@@ -11,6 +11,8 @@ st.set_up_config(
 
 # Custom CSS (optional - for styling )
 st.markdown('''
-                <style>
+               <style>
                 .main-header {font-size: 42px; font-weight: bold; text-align:center;}
                 .sub-header {font-size: 24px; text-align:center; color #700
+              </style>
+          ''', unsafe_allow_html = True)
