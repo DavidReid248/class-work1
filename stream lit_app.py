bf4fd1c7f8a2a1609894a5215b0b339220ebb77d
DavidReid248 <david.reid1@student.mec.cuny.edu>
2025-11-29 15:24:06 -0500

    Update stream lit_app.py

--- stream lit_app.py
+++ stream lit_app.py
@@ -87,4 +87,4 @@ elif page == '💼 Projects':
         col1, col2 = st.columns([1, 2])
 
     with col1:
-      st.image(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMmLQ1liZsB1dJTPSd_hBBdasqOFNXgv-7AA&s', use_column_width = True)
+      st.image(https://mir-s3-cdn-cf.behance.net/projects/808/a2f882224632473.Y3JvcCwxMzgwLDEwODAsMjcwLDA.jpg', use_column_width = True)
